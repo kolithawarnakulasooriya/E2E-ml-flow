9d70252c5f49c22e95b14307db8f6b6b6d0fb34d
kolitha <warnakulasooriyakolitha@gmail.com>
2025-03-31 18:29:09 -0400

    create abs class

---
+++ src/analysis/abs_missing_value_analysis.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+import pandas as pd
+
+class MissingValueAnalyzerTemplate(ABC):
+
+    @abstractmethod
+    def indeitify_missing_values(self, df: pd.DataFrame):
+        """_summary_
+        identify missing values in the dataframe
+
+        Args:
+            df (pd.DataFrame): input dataframe
+        """
+        pass
+    
+    @abstractmethod
+    def visualize_missing_values(self, df: pd.DataFrame):
+        """_summary_
+        visualize missing values in the dataframe
+
+        Args:
+            df (pd.DataFrame): _input dataframe
+        """
+        pass
+    
+    def analyse(self, df: pd.DataFrame):
+
+        """_summary_
+         pefrom the analysis tast for the dataframe
+
+         Args:
+            df (pd.DataFrame): _input dataframe
+        """
+        self.indeitify_missing_values(df)
+        self.visualize_missing_values(df)
