dffd69b35a0bb147cd7bebc8722a27e630642cd3
kolitha <warnakulasooriyakolitha@gmail.com>
2025-04-02 00:34:14 -0400

    update categorical analysis

--- src/analysis/varient_analysis.py
+++ src/analysis/varient_analysis.py
@@ -1,6 +1,6 @@
 from .abs_variate_analyse_strategy import UniVariantAnalyzeStategy
 from matplotlib import pyplot as plt
-from seaborn import histplot
+from seaborn import histplot, countplot
 import pandas as pd
 
 class NumericalUnivarientAnalyzer(UniVariantAnalyzeStategy):
@@ -33,4 +33,36 @@ class NumericalUnivarientAnalyzer(UniVariantAnalyzeStategy):
         plt.title(f"Distribution of {self._options}")
         plt.xlabel(self._options)
         plt.ylabel("F")
+        plt.show()
+
+class CategoricalUnivarientAnalyzer(UniVariantAnalyzeStategy):
+    """_summary_
+    plots the neumerical features of the dataset
+    """
+    def __init__(self):
+        self._options = ''
+
+    def setOptions(self, option: str):
+        """_summary_
+        set the data extracting feature
+        Args:
+             option (str): given feature of dataset
+
+        Returns:
+            UniVariantAnalyzeStategy: self
+        """
+        self._options = option
+        return self
+    
+    def analyse(self, df:pd.DataFrame):
+        """_summary_
+        plot the categorical feature distruibution of a dataset
+        Args:
+            df (pd.Dataframe): input dataset
+        """
+        plt.figure(figsize=(10,8))
+        countplot(x=self._options, data=df)
+        plt.title(f"Distribution of {self._options}")
+        plt.xlabel(self._options)
+        plt.ylabel("Count")
         plt.show()
--- tests/varient_analyse_test.py
+++ tests/varient_analyse_test.py
@@ -1,7 +1,7 @@
 import unittest
 import pandas as pd
 from unittest.mock import patch
-from src.analysis.varient_analysis import NumericalUnivarientAnalyzer
+from src.analysis.varient_analysis import NumericalUnivarientAnalyzer, CategoricalUnivarientAnalyzer
 
 class TestNumericalUnivarientAnalyzer(unittest.TestCase):
 
@@ -13,6 +13,26 @@ class TestNumericalUnivarientAnalyzer(unittest.TestCase):
     @patch('src.analysis.varient_analysis.histplot')
     def test_analyse(self, mock_figure, mock_title, mocl_x_label, mock_y_label, mock_show, mock_sns):
         strategy = NumericalUnivarientAnalyzer()
+        df = pd.DataFrame({'test': [1], 'name': ['test1']})
+        strategy.setOptions('test').analyse(df)
+        self.assertTrue(True)
+        mock_figure.assert_called()
+        mock_title.assert_called()
+        mocl_x_label.assert_called()
+        mock_y_label.assert_called()
+        mock_show.assert_called()
+        mock_sns.assert_called()
+
+class TestCategoricalUnivarientAnalyzer(unittest.TestCase):
+
+    @patch('src.analysis.varient_analysis.plt.figure')
+    @patch('src.analysis.varient_analysis.plt.title')
+    @patch('src.analysis.varient_analysis.plt.xlabel')
+    @patch('src.analysis.varient_analysis.plt.ylabel')
+    @patch('src.analysis.varient_analysis.plt.show')
+    @patch('src.analysis.varient_analysis.countplot')
+    def test_analyse(self, mock_figure, mock_title, mocl_x_label, mock_y_label, mock_show, mock_sns):
+        strategy = CategoricalUnivarientAnalyzer()
         df = pd.DataFrame({'test': [1], 'name': ['test1']})
         strategy.setOptions('test').analyse(df)
         self.assertTrue(True)
