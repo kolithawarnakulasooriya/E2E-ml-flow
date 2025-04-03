7ea71bb5539e34545c654aee39435b5a1058844e
kolitha <warnakulasooriyakolitha@gmail.com>
2025-04-03 10:06:26 -0400

    multi varient analysis

--- src/analysis/varient_analysis.py
+++ src/analysis/varient_analysis.py
@@ -1,6 +1,6 @@
-from .abs_variate_analyse_strategy import UniVariantAnalyzeStategy, BiVariantAnalyzeStategy
+from .abs_variate_analyse_strategy import UniVariantAnalyzeStategy, BiVariantAnalyzeStategy, MultiVariantAnalyzeStategyTemplate
 from matplotlib import pyplot as plt
-from seaborn import histplot, countplot,scatterplot, boxplot
+from seaborn import histplot, countplot,scatterplot, boxplot, heatmap, pairplot
 import pandas as pd
 
 class NumericalUnivarientAnalyzer(UniVariantAnalyzeStategy):
@@ -61,4 +61,18 @@ class CategoricalBiVarientAnalysis(BiVariantAnalyzeStategy):
         plt.title(f"Distribution of {self._options1} vs {self._options2}")
         plt.xlabel(self._options1)
         plt.ylabel(self._options2)
+        plt.show()
+
+class BasicMultiVarientAnalysis(MultiVariantAnalyzeStategyTemplate):
+
+    def generate_corr_heatmap(self, df):
+        plt.figure(figsize=(10,8))
+        heatmap(data=df.select_dtypes(include=['float64', 'int64']).corr(), annot=True, cmap='coolwarm', fmt='.2f')
+        plt.title(f"Correlation Heatmap")
+        plt.show()
+    
+    def generate_pairplot(self, df):
+        plt.figure(figsize=(10,8))
+        pairplot(data=df)
+        plt.title("Pairplot of Selected Features")
         plt.show()
--- tests/varient_analyse_test.py
+++ tests/varient_analyse_test.py
@@ -2,7 +2,7 @@ import unittest
 import pandas as pd
 from unittest.mock import patch
 from src.analysis.varient_analysis import NumericalUnivarientAnalyzer, \
-    CategoricalUnivarientAnalyzer, NumericalBiVarientAnalysis, CategoricalBiVarientAnalysis
+    CategoricalUnivarientAnalyzer, NumericalBiVarientAnalysis, CategoricalBiVarientAnalysis, BasicMultiVarientAnalysis
 
 class TestNumericalUnivarientAnalyzer(unittest.TestCase):
 
@@ -83,6 +83,54 @@ class TestCategoricalBiVarientAnalysis(unittest.TestCase):
         mock_y_label.assert_called()
         mock_show.assert_called()
         mock_sns.assert_called()
+
+class TestBasicMultiVarientAnalysis(unittest.TestCase):
+
+    @patch('src.analysis.varient_analysis.plt.figure')
+    @patch('src.analysis.varient_analysis.plt.title')
+    @patch('src.analysis.varient_analysis.plt.show')
+    @patch('src.analysis.varient_analysis.heatmap')
+    def test_generate_corr_heatmap(self, mock_figure, mock_title, mock_show, mock_sns):
+        strategy = BasicMultiVarientAnalysis()
+        df = pd.DataFrame({'test1': [1], 'test2': [2]})
+        strategy.generate_corr_heatmap(df)
+        self.assertTrue(True)
+        mock_figure.assert_called()
+        mock_title.assert_called()
+        mock_show.assert_called()
+        mock_sns.assert_called()
+
+    @patch('src.analysis.varient_analysis.plt.figure')
+    @patch('src.analysis.varient_analysis.plt.title')
+    @patch('src.analysis.varient_analysis.plt.show')
+    @patch('src.analysis.varient_analysis.heatmap')
+    @patch('src.analysis.varient_analysis.pairplot')
+    def test_generate_corr_heatmap(self, mock_figure, mock_title, mock_show, mock_sns_heatmap, mock_sns_pairplot):
+        strategy = BasicMultiVarientAnalysis()
+        df = pd.DataFrame({'test1': [1], 'test2': [2]})
+        strategy.analyse(df)
+        self.assertTrue(True)
+        mock_figure.assert_called()
+        mock_title.assert_called()
+        mock_show.assert_called()
+        mock_sns_heatmap.assert_called()
+        mock_sns_pairplot.assert_called()
+
+    @patch('src.analysis.varient_analysis.plt.figure')
+    @patch('src.analysis.varient_analysis.plt.title')
+    @patch('src.analysis.varient_analysis.plt.show')
+    @patch('src.analysis.varient_analysis.pairplot')
+    def test_analysis(self, mock_figure, mock_title, mock_show, mock_sns):
+        strategy = BasicMultiVarientAnalysis()
+        df = pd.DataFrame({'test1': [1], 'test2': [2]})
+        strategy.generate_pairplot(df)
+        self.assertTrue(True)
+        mock_figure.assert_called()
+        mock_title.assert_called()
+        mock_show.assert_called()
+        mock_sns.assert_called()
+
+
 
 if __name__ == '__main__':
     unittest.main()
